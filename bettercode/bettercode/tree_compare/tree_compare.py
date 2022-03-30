b311d467524249d0ccbea8a3e2034ee2c9bfad9b
Shaun Patterson <shaunpatterson@gmail.com>
2022-03-30 07:52:02 -0400

    Answers for TreeCompare

--- bettercode/bettercode/tree_compare/tree_compare.py
+++ bettercode/bettercode/tree_compare/tree_compare.py
@@ -9,4 +9,15 @@ class Node:
   right: Optional['Node']
 
 def compare_nodes(a: Optional[Node], b: Optional[Node]) -> bool:
-  return False
+  if not a and not b:
+    return True
+
+  if not a and b:
+    return False
+  if not b and a:
+    return False
+
+  left = compare_nodes(a.left, b.left)
+  right = compare_nodes(a.right, b.right)
+
+  return left and right and a.val == b.val
